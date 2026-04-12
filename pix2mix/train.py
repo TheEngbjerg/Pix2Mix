5e85140958b816d737cfb2d25c9d250c3d422441
Engbjerg <nicolaiengbjerg@gmail.com>
2026-04-12 17:58:52 +0200

    created rough training script

--- pix2mix/train.py
+++ pix2mix/train.py
@@ -1,5 +1,35 @@
 import torch
+from diffwave.inference import predict
 from torch import nn
 from model.encoder.pix_encoder import PixEncoder
 from data_utilities import get_dataloader
 import torch.nn.functional as F
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+epochs = 10
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model = PixEncoder().to(device)
+
+
+
+def train(model: PixEncoder):
+    dataloader = get_dataloader(dataset_dir="dataset_10/")
+    loss_fn = nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+    for batch in dataloader:
+        optimizer.zero_grad()
+        input = batch["input"].to(device)
+        target = batch["target"].to(device)
+        output = model(input)
+        loss = loss_fn(output, target)
+        loss.backward()
+        optimizer.step()
+    
+    return output
+
+for epoch in tqdm(range(epochs)):
+    sample = train(model=model)
+
+predict(sample, model_dir="diffwave-ljspeech-22kHz-1000578.pt", device=device)
