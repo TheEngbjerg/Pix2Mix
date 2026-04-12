a3dd1e05af3033802b39a0a3689f2a09ae4b636c
Nina Ngo <112858972+phoonhi@users.noreply.github.com>
2026-04-12 16:55:31 +0200

    encoder + train.py

--- pix2mix/model/encoder/pix_encoder.py
+++ pix2mix/model/encoder/pix_encoder.py
@@ -9,25 +9,38 @@ from torchvision import models
 
 
 class PixEncoder(nn.Module):
-    def __init__(self, n_mels=80):
+    def __init__(self, n_mels=80, T=62):
         super().__init__()
         self.n_mels = n_mels
+        self.T = T
+
         resnet = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
 
-        self.backbone = nn.Sequential(*list(resnet.children())[:-1])
+        self.backbone = nn.Sequential(
+            *list(resnet.children())[:-1]
+        )  # last dimension (batch, 512, 1, 1)
 
-        self.projection = nn.Linear(512, n_mels)
+        self.projection = nn.Linear(512, self.n_mels * T)
+
+        # # Custom layers to map to spectrogram dimensions
+        # self.upsample = nn.Sequential(
+        #     nn.ConvTranspose2d(
+        #         512, 256, kernel_size=3, stride=2, padding=1, output_padding=1
+        #     ),
+        #     nn.ReLU(),
+        #     nn.ConvTranspose2d(
+        #         256, 1, kernel_size=3, stride=2, padding=1, output_padding=1
+        #     ),
+        #     nn.Sigmoid(),  # For amplitude values between 0 and 1
+        # )
 
     def forward(self, image):
-
         x = self.backbone(image)  # (batch, 512, 1, 1)
+        x = x.squeeze(-1).squeeze(-1)  # (batch, 512)
+        x = self.projection(x)  # (batch, n_mels * 62)
+        x = x.view(-1, self.n_mels, self.T)  # (batch, n_mels, 62)
 
-        # Linear expect 2D --> needs to squuze out the two 1 dimensions
-        x = x.squeeze(-1).squeeze(-1)
-
-        x = self.projection(x)
-
-        # unsqueeze to fit with the diffwave dimensions
-        x = x.unsqueeze(-1)  # (batch, n_mels, 1)
+        # transform normalized to [0,1] --> need same constrain
+        x = torch.sigmoid(x)
 
         return x
---
+++ pix2mix/train.py
@@ -0,0 +1,5 @@
+import torch
+from torch import nn
+from model.encoder.pix_encoder import PixEncoder
+from data_utilities import get_dataloader
+import torch.nn.functional as F
