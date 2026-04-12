f612eafd1f0abab6a82a2f1ea8dd9129e908197c
Nina Ngo <112858972+phoonhi@users.noreply.github.com>
2026-04-12 13:01:57 +0200

    Insert default dataset_dir for ai-lab

--- pix2mix/data_utilities.py
+++ pix2mix/data_utilities.py
@@ -7,7 +7,7 @@ import torchaudio
 
 
 class JamendoDataset(Dataset):
-    def __init__(self, dataset_dir="/ceph/project/pix_audio/data/dataset_jamendo"):
+    def __init__(self, dataset_dir):
         super().__init__()
         self.dataset_dir = dataset_dir
         self.folders = [
@@ -33,6 +33,8 @@ class JamendoDataset(Dataset):
         return image, audio
 
 
-def get_dataloader(dataset_dir, batch_size=16):  # just 16 for now
+def get_dataloader(
+    dataset_dir="/ceph/project/pix_audio/data/dataset_jamendo", batch_size=16
+):  # just 16 for now
     dataset = JamendoDataset(dataset_dir)
     return DataLoader(dataset, batch_size=batch_size, shuffle=True)
