1701da7c5dd8ae753a3ef38af305aae0550b1f39
Nina Ngo <112858972+phoonhi@users.noreply.github.com>
2026-04-12 17:53:06 +0200

    add cross validation

--- pix2mix/data_utilities.py
+++ pix2mix/data_utilities.py
@@ -1,11 +1,11 @@
 import os
 import numpy as np
-import torch
-from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from PIL import Image
 import torchaudio
 from model.decoder.diffwave.src.diffwave.preprocess import transform
+from torch.utils.data import Dataset, DataLoader, Subset
+from sklearn.model_selection import KFold
 
 
 class JamendoDataset(Dataset):
@@ -33,16 +33,26 @@ class JamendoDataset(Dataset):
 
         if not os.path.exists(np_path):
             transform(audio_path)
-        
-        spectrogram = torch.from_numpy(
-            np.load(np_path)
-        )
+
+        spectrogram = torch.from_numpy(np.load(np_path))
 
         return image, spectrogram
 
 
 def get_dataloader(
-    dataset_dir="/ceph/project/pix_audio/data/dataset_jamendo", batch_size=16
+    dataset_dir="/ceph/project/pix_audio/data/dataset_jamendo", batch_size=16, k=5
 ):  # just 16 for now
     dataset = JamendoDataset(dataset_dir)
-    return DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    kf = KFold(n_splits=k, shuffle=True, random_state=42)
+
+    folds = []
+    for train_indices, val_indices in kf.split(dataset):
+        train_loader = DataLoader(
+            Subset(dataset, train_indices), batch_size=batch_size, shuffle=True
+        )
+        val_loader = DataLoader(
+            Subset(dataset, val_indices), batch_size=batch_size, shuffle=False
+        )
+        folds.append((train_loader, val_loader))
+
+    return folds
